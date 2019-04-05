79f931e85817bc8fdcb34531c9b1aaf107077135
Henrik Nøsen Dahle <hnd007@hotmail.com>
2019-04-05 21:38:19 +0200

    Endret config.h og swarm_board.h

--- Drivers/Include/swarm_board.h
+++ Drivers/Include/swarm_board.h
@@ -7,4 +7,23 @@
  *
  *
  */
+#include "config.h"
 
+#define NRF_GPIO_PIN_MAP(port, pin) ((port << 5) | (pin & 0x1F))
+
+#define DUAL_CHIP_UART_RX_PIN NRF_GPIO_PIN_MAP(0,0)
+#define DUAL_CHIP_UART_TX_PIN NRF_GPIO_PIN_MAP(0,0)
+
+#ifdef SECONDARY_CHIP
+#if SECONDARY_CHIP
+
+
+
+#else
+
+
+
+#endif
+#else
+
+#endif
--- config.h
+++ config.h
@@ -13,7 +13,7 @@
 /////// Dual_chip settings////////////
 #define DUAL_CHIP_ENABLE false // enable dual chip
 
-#ifdef DUAL_CHIP_ENABLE
+#if DUAL_CHIP_ENABLE
     #define SECONDERY_CHIP false
 #endif
 
