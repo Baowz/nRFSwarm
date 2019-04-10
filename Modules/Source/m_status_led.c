f400f26847d3f32e91b4b08afa6604220da4ab46
Henrik Nøsen Dahle <hnd007@hotmail.com>
2019-04-10 20:28:27 +0200

    Updated m_status_led

--- Modules/Source/m_status_led.c
+++ Modules/Source/m_status_led.c
@@ -3,5 +3,13 @@
  *
  *
  */
+#include "m_status_led.h"
 
-
+void status_led_init(status_led_t *status_led, int address_r, int address_g, int address_b){
+    status_led -> r_pin = address_r;
+    status_led -> g_pin = address_g;
+    status_led -> b_pin = address_b;
+    status_led -> r = 0;
+    status_led -> g = 0;
+    status_led -> b = 0;
+}
--- Modules/include/m_status_led.h
+++ Modules/include/m_status_led.h
@@ -3,4 +3,35 @@
  *
  *
  */
+#include "config.h"
+
+typedef struct {
+    int r;
+    int g;
+    int b;
+    int r_pin;
+    int g_pin;
+    int b_pin;
+}status_led_t;
+
+void status_led_init(status_led_t *status_led, int address_r, int address_g, int address_b);
+
+#if DUAL_CHIP_ENABLE
+#if SECONDARY_CHIP
+
+    status_led_t status_led_1;
+    status_led_t status_led_2;
+
+#else
+
+    status_led_t status_led;
+
+
+#endif
+#else
+
+    status_led_t status_led;
+
+
+#endif
 
