a59570a62313467e58565ed4d781989721f05be1
henriknd <hnd007@hotmail.com>
2019-04-25 19:02:22 +0200

    simple updates

---
+++ Drivers/Include/d_BMX160_twi.h
@@ -0,0 +1,5 @@
+
+
+
+#define BMX160_ADDR 0x68
+status_code_t BMX160_INIT(void);
--- Drivers/Include/d_twi.h
+++ Drivers/Include/d_twi.h
@@ -1,4 +1,8 @@
 #include <stdint.h>
 
 
-#define IS31FL3737_ADDR 0xA0
+typedef enum{
+    connected = 0x00,
+    not_found = 0x01,
+    error     = 0x02
+}status_code_t;
