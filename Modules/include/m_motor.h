b6ccac4d3a6e63ba25dc76cb18237b32030fdf37
Berntovs <41931589+Berntovs@users.noreply.github.com>
2019-05-20 11:11:30 +0200

    Added localisation

--- Modules/include/m_motor.h
+++ Modules/include/m_motor.h
@@ -2,27 +2,57 @@
 #include <stdint.h>
 #include <string.h>
 #include "nrf_gpio.h"
+//#include "nrf_drv_pwm.h"
+#include "nrfx_pwm.h"
+#include "nrf_pwm.h"
 
-#include "d_swarm_board.h"
 
-#define PWM_PIN_A MOTOR_PWM_A_PIN
-#define PWM_PIN_B MOTOR_PWM_B_PIN
-#define MOTOR_PIN_A1 MOTOR_AIN_1_PIN
-#define MOTOR_PIN_A2 MOTOR_AIN_2_PIN
-#define MOTOR_PIN_B1 MOTOR_BIN_1_PIN
-#define MOTOR_PIN_B2 MOTOR_BIN_2_PIN
 
+
+
+// Define Pin for PWM motor control
+#define PWM_PIN_A               NRF_GPIO_PIN_MAP(0,03)    // Pin P0.04 Selected
+//#define PWM_PIN_0               NRF_GPIO_PIN_MAP(0,13)    // Switch to led
+#define PWM_PIN_B               NRF_GPIO_PIN_MAP(0,04)    // Pin P0.04 Selected
+#define MOTOR_PIN_A1            NRF_GPIO_PIN_MAP(0,28)    // Pin P0.04 Selected
+#define MOTOR_PIN_A2            NRF_GPIO_PIN_MAP(0,29)    // Pin P0.04 Selected
+#define MOTOR_PIN_B1            NRF_GPIO_PIN_MAP(0,30)    // Pin P0.04 Selected
+#define MOTOR_PIN_B2            NRF_GPIO_PIN_MAP(0,31)    // Pin P0.04 Selected
+
+//#define LED_1          NRF_GPIO_PIN_MAP(0,13)
+#define LED_3          NRF_GPIO_PIN_MAP(0,15)
+#define LED_4          NRF_GPIO_PIN_MAP(0,16)
+
+
+//Select direction
+// Stop
+// 1 Forward
+// 2 Reverse
+// 3 Turn CW
+// 4 Turn CCW
 typedef struct
 {
-  bool motor0forward;
-  bool motor1forward;
-} motor_direction_t;
+  int direction;
+}motor_direction_t;
 
 typedef struct
 {
   uint16_t speedA;
   uint16_t speedB;
-} motor_speed_t;
+}motor_speed_t;
+
+
+//Initial motor direction and speed
+ static motor_direction_t selectdirection =
+ {
+ .direction = 0,
+ };
+
+ static motor_speed_t speed =
+ {
+ .speedA = 0,
+ .speedB = 0,
+ };
 
 void motor_direction(motor_direction_t *direction);
 void motor_speed(motor_speed_t *speed);
