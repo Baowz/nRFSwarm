3438dde98123853eb9df0c0bedb44d1a92344f71
Berntovs <41931589+Berntovs@users.noreply.github.com>
2019-05-20 11:07:44 +0200

    temp

--- Modules/include/m_motor.h
+++ Modules/include/m_motor.h
@@ -39,3 +39,33 @@ void motor_speed(motor_speed_t *speed);
 void init_motor_pwm(void);
 
 void motor_run(void);
+
+
+
+//Example main
+// int main(void)
+// {
+
+//         //Create struct named motors and insert values
+//     init_motor_pwm();
+    
+//     static motor_direction_t direction =
+//     {
+//     .motor0forward = 0,
+//     .motor1forward = 1
+//     };
+
+//     static motor_speed_t speed =
+//     {
+//     .speedA = 100,
+//     .speedB = 20,
+//     };
+
+//     for (;;) // Neverending loop
+//    {      
+//       motor_direction(&direction);
+//       motor_speed(&speed);
+//       motor_run();
+  
+//    }
+// }
